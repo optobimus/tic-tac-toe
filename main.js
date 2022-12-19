548045faa699c09329ce651ce60d97039b07b8a2
Tobias <113835194+optobimus@users.noreply.github.com>
2022-12-19 21:13:31 +0100

    Add functionality for click on board items Also, make design responsive for mobile devices

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 const gameBoard = (() => {
-    let _board = new Array(9);
+    const board = new Array(9);
+
+    const reset = () => {
+        for (let i = 0; i < board.length; i++) {
+            board[i] = "";
+        }
+    }
 })();
 
 const displayController = (() => {
+    const fieldElements = document.querySelectorAll('.field');
 
+    fieldElements.forEach((field) => 
+        field.addEventListener('click', (e) => {
+            e.target.textContent = "X";
+        })
+    );
+      
+})();
+
+const gameController = (() => {
+    const playerX = player("X");
+    const playerY = player("Y");
 })();
 
 const player = (sign) => {
