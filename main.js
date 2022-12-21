c09bd6748ba3f2c0259fe1978f26c33fa6085e9f
Tobias <113835194+optobimus@users.noreply.github.com>
2022-12-21 20:50:47 +0100

    Add Tie functionality

--- main.js
+++ main.js
@@ -57,8 +57,10 @@ const displayController = (() => {
     };
 
     const updateDisplayMessage = (winner) => {
-        if (gameController.checkWin()) {
+        if (gameController.checkWin() === "win") {
             message.textContent = "The winner is Player " + winner + "!";
+        } else if (gameController.checkWin() === "tie"){
+            message.textContent = "TIE!";
         } else {
             message.textContent = gameController.updateMessageContent();
         }
@@ -81,10 +83,7 @@ const gameController = (() => {
 
     const playRound = (index) => {
         gameBoard.setField(index, getCurrentPlayer());
-        displayController.updateDisplayMessage();
-        if (checkWin(index)) {
-            displayController.updateDisplayMessage(setWinner());
-        }
+        displayController.updateDisplayMessage(setWinner());
         round++;
     };
 
@@ -97,25 +96,30 @@ const gameController = (() => {
         return round % 2 === 1 ? "Player O's turn" : "Player X's turn";
     }
 
-    const checkWin = (index) => {
+    const checkWin = () => {
         if (gameBoard.getField(0) === gameBoard.getField(1) && gameBoard.getField(0) === gameBoard.getField(2) && gameBoard.getField(0) !== "") {
-            return true;
+            return "win";
         } else if (gameBoard.getField(3) === gameBoard.getField(4) && gameBoard.getField(3) === gameBoard.getField(5) && gameBoard.getField(3) !== "") {
-            return true;
+            return "win";
         } else if (gameBoard.getField(6) === gameBoard.getField(7) && gameBoard.getField(6) === gameBoard.getField(8) && gameBoard.getField(6) !== "") {
-            return true;
+            return "win";
         } else if (gameBoard.getField(0) === gameBoard.getField(3) && gameBoard.getField(0) === gameBoard.getField(6) && gameBoard.getField(0) !== "") {
-            return true;
+            return "win";
         } else if (gameBoard.getField(1) === gameBoard.getField(4) && gameBoard.getField(1) === gameBoard.getField(7) && gameBoard.getField(1) !== "") {
-            return true;   
+            return "win";   
         } else if (gameBoard.getField(2) === gameBoard.getField(5) && gameBoard.getField(2) === gameBoard.getField(8) && gameBoard.getField(2) !== "") {
-            return true;
+            return "win";
         } else if (gameBoard.getField(0) === gameBoard.getField(4) && gameBoard.getField(0) === gameBoard.getField(8) && gameBoard.getField(0) !== "") {
-            return true;
+            return "win";
         } else if (gameBoard.getField(2) === gameBoard.getField(4) && gameBoard.getField(2) === gameBoard.getField(6) && gameBoard.getField(2) !== "") {
-            return true;
+            return "win";
         } else {
-            return false;
+            for (let index = 0; index < 9; index++) {
+                if (gameBoard.checkFree(index) === true) {
+                    return;
+                }
+            }
+            return "tie";
         }
     }
 
